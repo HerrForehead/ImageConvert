c8cae4f2851c1425bdc009a3dfdd7c65545060c4
SvenVerstegen <verstegensven@gmail.com>
2024-04-02 14:16:45 +0200

    Version 1.0

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, send_file
 import os
 import subprocess
 from PIL import Image 
@@ -11,66 +11,72 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def upload_image():
     if request.method == 'POST':
-        # Check if the POST request has a file part
         if 'file' not in request.files:
             return redirect(request.url)
         
         file = request.files['file']
 
-        # Check if the user selected a file
         if file.filename == '':
             return redirect(request.url)
 
-        # Check if the file has a valid extension
         allowed_extensions = ['.png', '.jpg', '.jpeg']
         if not any(file.filename.lower().endswith(ext) for ext in allowed_extensions):
             return 'Invalid file type. Only .png or .jpg/.jpeg files are allowed.'
 
-        # Save the file to the inputImage folder with the name "input"
         filename = 'input' + os.path.splitext(file.filename)[1]
         file.save(os.path.join('inputImage', filename))
 
-        print("1")
-
-        # Resize image if larger than 122 x 250
         process_image(f"./inputImage/input"+ os.path.splitext(file.filename)[1])
-        # Remove the original image after resizing
+
         os.remove(f"./inputImage/input"+ os.path.splitext(file.filename)[1])
 
-        # Convert the image to a 3-color BMP image
         commandimg = f"convert .\\inputImage\\processed_image.png -dither FloydSteinberg -define dither:diffusion-amount=85% -remap eink-3color.png -depth 4 BMP3:.\imgToBMP\output.bmp"
         os.system(commandimg)
-        # Remove the processed image after conversion
+
         os.remove(f".\\inputImage\\processed_image.png")
 
-        # Change output.bmp to 4 bit depth
         img = Image.open(".\\imgToBMP\\output.bmp")
         img = img.convert("P", palette=Image.ADAPTIVE, colors=4)
         img.save(".\\imgToBMP\\output.bmp")
 
-        # Convert images to black and red format.
         convert_red(f".\\imgToBMP\\output.bmp", ".\\output\\output_red.bmp")
         convert_black(f".\\imgToBMP\\output.bmp", ".\\output\\output_black.bmp")
 
-        # Convert the images to 4 bit depth
         img = Image.open(".\\output\\output_red.bmp")
         img = img.convert("P", palette=Image.ADAPTIVE, colors=4)
         img.save(".\\output\\output_red.bmp")
 
-        # Do the same for the black image
         img = Image.open(".\\output\\output_black.bmp")
         img = img.convert("P", palette=Image.ADAPTIVE, colors=4)
         img.save(".\\output\\output_black.bmp")
 
-        # Remove the original BMP file after conversion
         os.remove(".\\imgToBMP\\output.bmp")
 
-        # Turn output_black.bmp and output_red.bmp into byte arrays using bmp2hex
         os.system(f"powershell python.exe ./bmp2array.py ./output/output_black.bmp, ./output/output_red.bmp > ./output/Graphics.h")
         
-        return 'File uploaded successfully!'
+        # Get the paths for the preview images and Graphics.h
+        preview_red = "/output/output_red.bmp"
+        preview_black = "/output/output_black.bmp"
+        graphics_h = "/downloadfile"
+        
+        # Pass the paths to the template
+        return render_template('index.html', preview_red=preview_red, preview_black=preview_black, graphics_h=graphics_h)
     
     return render_template('index.html')
+
+# Sending the file to the user
+@app.route('/downloadfile')
+def download():
+   return send_file('./output/Graphics.h', as_attachment=True)
+
+# Send the preview images to the user
+@app.route('/output/output_red.bmp')
+def preview_red():
+    return send_file('./output/output_red.bmp')
+
+@app.route('/output/output_black.bmp')
+def preview_black():
+    return send_file('./output/output_black.bmp')
 
 if __name__ == '__main__':
     app.run()
